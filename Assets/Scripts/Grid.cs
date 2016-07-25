ddbb66ad7b6b3da85603dea57188ce7e7a560e9d
Diego Machado <diegomac@gmail.com>
2016-07-25 20:17:49 -0300

    Graphics + Basic Grid Features

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour 
+{
+	public const int EMPTY = 0;
+	public const int CIRCLE = 1;
+	public const int SQUARE = 2;
+
+	public bool isPlayerTurn = true;
+
+	public Sprite playerPiece;
+	public Sprite computerPiece;
+
+	public int[] spaces = new int[9];
+	public Button[] spacesImages = new Button[9];
+
+	void Start()
+	{
+		spacesImages = transform.Find("Spaces").GetComponentsInChildren<Button>();
+	}
+		
+	public void SetSpacePiece(Button piece) 
+	{
+		Debug.Log(piece.image.sprite);
+
+		if(isPlayerTurn)
+		{
+			piece.image.sprite = playerPiece;
+			var color = piece.image.color;
+			color.a = 1;
+			piece.image.color = color;
+			piece.enabled = false;
+		} 
+		else
+		{
+			piece.image.sprite = computerPiece;
+			var color = piece.image.color;
+			color.a = 1;
+			piece.image.color = color;
+			piece.enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Piece : MonoBehaviour 
+{
+	void Start () 
+	{
+	}
+
+	public void SetPiece() 
+	{
+		
+	}
+}
